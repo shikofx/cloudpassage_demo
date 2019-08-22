ce58cb207debdd325502c264148b89a0324ab157
Dzmitry Parkheichuk <Dzmitry_Parkheichuk@epam.com>
2019-08-22 14:21:59 +0300

    PARAM: try to using of nightwatch-data-driven tests. Issue: cannot create property 'name' on string 'Browser not initialized'

--- data/partnerData.js
+++ data/partnerData.js
@@ -1,9 +1,9 @@
 module.exports = {
-    firstName: 'Test',
-    lastName: 'Partner',
-    email: 'testpartner@test.partner',
-    companyName: 'Test Company',
-    phone: '+375 29 222 22 22',
-    department: 'Support',
-    comments: 'Test comments'
+    firstName:      ['Test',                        'Test1'],
+    lastName:       ['Partner',                     'Partner1'],
+    email:          ['testpartner@test.partner',    'testpartner1@test.partner'],
+    companyName:    ['Test Company',                'Test Company 1'],
+    phone:          ['+375 29 222 22 22',           '+375 29 222 22 21'],
+    department:     ['Support',                     'Support'],
+    comments:       ['Test comments',               'Test comments']
 };
--- tests/contactWith/sendPartnerData.js
+++ tests/contactWith/sendPartnerData.js
@@ -1,10 +1,11 @@
+var DataDrivenTest = require('nightwatch-data-driven').default;
+// require('nightwatch-data-driven').DataDrivenTest;
 var contactPage;
-var partnerData;
-
+var partnerData = require('../../data/partnerData');
+// var partnerData = browser.globals.partnerData;
 module.exports = {
-    
     before: function(browser){
-        partnerData = browser.globals.partnerData;
+        // 
         contactPage = browser.page.contactPage();
     },
 
@@ -20,20 +21,39 @@ module.exports = {
     after: function(browser){
         browser.end();
     },
-
     '@tags': ['filling', 'smoke', 'positive', 'contact'],
-    'send valid partner data:' () {
-        
-        contactPage
-            .fillPartnerForm(partnerData)
-            .switchToRecaptchaFrame()
-            .recaptchaSelect()
-        .expect.element('@recaptcha').to.be.selected;
+    'send valid partner data:' (browser) {
 
-        contactPage
-            .switchToParentFrame()
-            .submit()
-        .section.partnerForm
-        .expect.element('@thanks').text.to.contain("Thank you for your submission");
+        console.log('Before DataDrivenTest');
+
+        new DataDrivenTest(browser, function(data, name){
+            console.log('Into DataDrivenTest');
+            this.contactPage
+                .fillPartnerForm(data)
+                .switchToRecaptchaFrame()
+                .recaptchaSelect();
+            this.browser.page.workspace().assertIsCurrentPage(name);
+            browser.expect.element('@recaptcha').to.be.selected;
+
+            contactPage
+                .switchToParentFrame()
+                .submit()
+            .section.partnerForm
+            .expect.element('@thanks').text.to.contain("Thank you for your submission");
+        })
+        .forCases({
+            "1.": {   
+                    firstName:      partnerData.firstName, 
+                    lastName:       partnerData.lastName,
+                    email:          partnerData.email,
+                    phone:          partnerData.phone,
+                    companyName:    partnerData.companyName,
+                    comments:       partnerData.comments,
+                    department:     partnerData.department
+                }
+             }, 'name1' );
+        console.log('after forcases test');
+        }
+
+        
     }
-}
