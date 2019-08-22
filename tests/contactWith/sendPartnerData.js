09d0ff9b027686a55b1782e369060c2dbf89730c
Dzmitry Parkheichuk <Dzmitry_Parkheichuk@epam.com>
2019-08-22 17:18:41 +0300

    Add parameters wit ndd. Comment string self.arrange(function () { in ndd.js

--- tests/contactWith/sendPartnerData.js
+++ tests/contactWith/sendPartnerData.js
@@ -1,7 +1,7 @@
-var DataDrivenTest = require('nightwatch-data-driven').default;
+let DataDrivenTest = require('nightwatch-data-driven').default;
 // require('nightwatch-data-driven').DataDrivenTest;
-var contactPage;
-var partnerData = require('../../data/partnerData');
+let contactPage;
+let partnerData = require('../../data/partnerData');
 // var partnerData = browser.globals.partnerData;
 module.exports = {
     before: function(browser){
@@ -25,35 +25,40 @@ module.exports = {
     'send valid partner data:' (browser) {
 
         console.log('Before DataDrivenTest');
-
-        new DataDrivenTest(browser, function(data, name){
-            console.log('Into DataDrivenTest');
-            this.contactPage
-                .fillPartnerForm(data)
-                .switchToRecaptchaFrame()
-                .recaptchaSelect();
-            this.browser.page.workspace().assertIsCurrentPage(name);
-            browser.expect.element('@recaptcha').to.be.selected;
-
-            contactPage
-                .switchToParentFrame()
-                .submit()
-            .section.partnerForm
-            .expect.element('@thanks').text.to.contain("Thank you for your submission");
-        })
-        .forCases({
-            "1.": {   
-                    firstName:      partnerData.firstName, 
-                    lastName:       partnerData.lastName,
-                    email:          partnerData.email,
-                    phone:          partnerData.phone,
-                    companyName:    partnerData.companyName,
-                    comments:       partnerData.comments,
-                    department:     partnerData.department
+        let tests = new DataDrivenTest()
+            .withBrowser(browser)
+            .withArrange(function(){
+                return contactPage.clearPartnerForm();
+            })
+            .withAct(function(data, name){
+                console.log('Into act');
+                contactPage
+                    .fillPartnerForm(data).pause(5000)
+                    // .switchToRecaptchaFrame()
+                    // .recaptchaSelect();
+            })
+            .withAssert(function(){
+                console.log("Into assert");
+                browser.page.contactPage().assert.urlContains('contact');
+            // this.contactPage.expect.element('@recaptcha').to.be.selected;
+            })
+            .forCases({
+                "Positive data":partnerData.valid,
+                "Unvalid  data":partnerData.unvalid
                 }
-             }, 'name1' );
-        console.log('after forcases test');
-        }
-
+            );    
+            // contactPage
+            //     .switchToParentFrame()
+            //     .submit()
+            // .section.partnerForm
+            // .expect.element('@thanks').text.to.contain("Thank you for your submission");
+        // })
+        // 
+        // console.log('after forcases test');
+        // )
+        // tests.withArrange('');
+        // tests.withAct('');
+        // tests.withAssert('')
         
     }
+}
